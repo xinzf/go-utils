442cb0affe7be5e4183494f491d79768217721f9
xiangzhi <xiangzhi@126.com>
2020-06-24 13:58:39 +0800

    增加功能

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/xinzf/go-utils
+
+go 1.14
+
+require (
+	github.com/cstockton/go-conv v0.0.0-20170524002450-66a2b2ba36e1
+	github.com/fatih/structs v1.1.0
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
+	github.com/satori/go.uuid v1.2.0
+	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
+)
--- tools.go
+++ tools.go
@@ -8,6 +8,8 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/cstockton/go-conv"
+	"github.com/fatih/structs"
 	"github.com/satori/go.uuid"
 )
 
@@ -62,4 +64,31 @@ func TimeFormat(t time.Time) string {
 
 func UUID() string {
 	return strings.Replace(uuid.NewV4().String(), "-", "", -1)
+}
+
+// StructToMap 转换struct为map
+func StructToMap(s interface{}) map[string]interface{} {
+	return structs.Map(s)
+}
+
+// StringToInt 字符串转数值
+func StringToInt(s string) (int64, error) {
+	return strconv.ParseInt(s, 10, 64)
+}
+
+func UnixMilli(t time.Time) int64 {
+	return t.UnixNano() / 1e6
+}
+
+func UnixMilliToTime(mse int64) time.Time {
+	str, _ := conv.String(mse)
+	var (
+		s  int64
+		ms int64
+	)
+	s, _ = conv.Int64(str[:10])
+	if len(str) >= 13 {
+		ms, _ = conv.Int64(str[10:])
+	}
+	return time.Unix(s, ms)
 }
